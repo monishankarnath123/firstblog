1e5432f61a52d8177d2795d8dece9840d528d39b
Monishankar Nath <57766568+monishankarnath123@users.noreply.github.com>
2020-02-22 13:35:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<?php 
+include_once "db.php";
+?>
+<html lang="en">
+  <head>
+    <title>MONI SHANKAR NATH</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--<link href="https://fonts.googleapis.com/css?family=Muli:300,400,700|Playfair+Display:400,700,900" rel="stylesheet">-->
+    <link rel="icon" href="images/icon.jpg" type="image/gif" sizes="16x16">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+  
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+    
+    <header class="site-navbar" role="banner">
+      <div class="container-fluid">
+        <div class="row align-items-center">
+          
+          <div class="col-12 search-form-wrap js-search-form">
+            <form method="get" action="#">
+              <input type="text" id="s" class="form-control" placeholder="Search what you want to know??">
+              <button class="search-btn" type="submit"><span class="icon-search"></span></button>
+            </form>
+          </div>
+
+          <div class="col-4 site-logo">
+            <a href="index.php" class="text-black h2 mb-0">MONI'S BLOG</a>
+          </div>
+
+          <div class="col-8 text-right">
+            <nav class="site-navigation" role="navigation">
+              <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block mb-0">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Tech</a></li>
+                <li><a href="#">Entertainment</a></li>
+                <li><a href="#">Travel</a></li>
+                <li><a href="#">Family & Friends</a></li>
+                <li class="d-none d-lg-inline-block"><a href="#" class="js-search-toggle"><span class="icon-search"></span></a></li>
+              </ul>
+            </nav>
+            <a href="#" class="site-menu-toggle js-menu-toggle text-black d-inline-block d-lg-none"><span class="icon-menu h3"></span></a></div>
+          </div>
+
+      </div>
+    </header>
+    
+    
+    <div class="site-cover site-cover-sm same-height overlay single-page" style="background-image: url('images/background.jpg');">
+      <div class="container">
+        <div class="row same-height justify-content-center">
+          <div class="col-md-12 col-lg-10">
+            <div class="post-entry text-center">
+              <h1 class="">WELCOME TO MY WEBSITE</h1>
+              <p class="lead mb-4 text-white">"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain…”</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    
+    <div class="site-section bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 order-md-2">
+            <img src="images/moni_1.jpg" alt="Image" class="img-fluid">
+          </div>
+          <div class="col-md-5 mr-auto order-md-1">
+            <h2>I Love To Explore</h2>
+            <p>I love to explore since I learned that "Exploring new things makes a man perfect rather than practicing the wrong things again and again". I'm a very # kind of person. I love to cook food, play football and making new friends &#128513.</p>
+            <ul class="ul-check list-unstyled success">
+              <li>Good food, Good Life.</li>
+              <li>Football is the best.</li>
+              <li>Family and Good Friends are equally important.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row mb-5 justify-content-center">
+          <div class="col-md-5 text-center">
+            <h2>Wanna Meet Me??</h2>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2082.111875204799!2d88.43454476815913!3d22.60858134360254!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a027586fd2fe159%3A0x906a4d6592adb154!2sTrishila%20Anandmai%20Mandir!5e0!3m2!1sen!2sin!4v1581862892745!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+            <p>J/M/9 Vidyasagarpally, Aswininagar, Baguiati, Kolkata-700159.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6 col-lg-4 mb-5 text-center">
+            <img src="images/maamee.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-4">
+            <h2 class="mb-3 h5">ANJANA NATH</h2>
+            <p>My beautiful mother who really cooks fantastic foods. She is my inspiration for everything I'm right now. Love U Mum Mum &#128536.</p>
+
+            <p class="mt-5">
+              <a href="https://www.facebook.com/anjana.nath.733" class="p-3"><span class="icon-facebook"></span></a>
+              <!--<a href="#" class="p-3"><span class="icon-instagram"></span></a>
+              <a href="#" class="p-3"><span class="icon-twitter"></span></a>-->
+            </p>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-5 text-center">
+            <img src="images/papa.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-4">
+            <h2 class="mb-3 h5">SANJAY NATH</h2>
+            <p>My handsome father. Everything I have is all his hardwork. Thank you for everything Babu.</p>
+
+            <p class="mt-5">
+              <a href="https://www.facebook.com/profile.php?id=100007862389024" class="p-3"><span class="icon-facebook"></span></a>
+              <!--<a href="#" class="p-3"><span class="icon-instagram"></span></a>
+              <a href="#" class="p-3"><span class="icon-twitter"></span></a>-->
+            </p>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-5 text-center">
+            <img src="images/bhai.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-4">
+            <h2 class="mb-3 h5">SUBHAM NATH</h2>
+            <p>He is my cutest and sweetest brother. He came to this world when I was in class 9. He is like my own child and I love him more than anything after my mother. Cheer up Hanuman &#128540.</p>
+
+            <p class="mt-5">
+             <!--<a href="#" class="p-3"><span class="icon-instagram"></span></a>
+              <a href="#" class="p-3"><span class="icon-twitter"></span></a>-->
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="site-section bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="images/family.jpg" alt="Image" class="img-fluid">
+          </div>
+          <div class="col-md-5 ml-auto">
+            <h2>Learn About Us</h2>
+            <p class="mb-4">Starting with my Father: He owns a Small-Scale Machinery Industry and a good Mechanical Engineer. To know more go to the "Friends & Family" Tab.<br>My Mother: She is a housemaker, But from 2018 I've opened a YouTube Channel named on her name. She assist me with everything and almost most of the content is on her. She cooks new and old recipes and present to the world through this channel. To know more go to the "Friends & Family" Tab.<br>My Brother:Currently he is in class 2. He love Drawing, playing footbal and his favourite toys are of Dinasour Family.To know more go to the "Friends & Family" Tab.<br>And last about Myself: The whole webpage is all about me. So find it and know more interesting stuffs about me&#128527. To know more go to the "Friends & Family" Tab.</p>
+            
+            <ul class="ul-check list-unstyled success">
+              <li>Father: Businessman.</li>
+              <li>Mother: Housemaker/YouTuber.</li>
+              <li>Brother: School student.</li>
+                <li>Me: IT Engineer.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="site-section bg-white">
+      <div class="container">
+        <div class="row justify-content-center text-center">
+          <div class="col-md-5">
+            <div class="subscribe-1 ">
+              <h2>Let Me Know!</h2>
+              <p class="mb-5">Let me know how much you like my website. Please enter your valid MailID. And don't forget to mention what you like and what you don't like. Your feedbacks are always welcome. </p>
+                              <form action="#">
+                <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email address"> 
+                  <textarea name="message" id="message" cols="30" rows="7" class="form-control" placeholder="Write your notes or questions here..."></textarea>
+                <input type="submit" class="btn btn-primary" id="submit" name="submit" value=" Submit! ">
+    </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+      
+    <div class="site-footer">
+      <div class="container">
+        <div class="row mb-5">
+         
+          <div class="col-md-4">
+            <!-- <h3 class="footer-heading mb-4">Navigation</h3> -->
+            <ul class="list-unstyled float-left mr-5">
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">My Career</a></li>
+            </ul>
+            <ul class="list-unstyled float-left">
+              <li><a href="#">Travel</a></li>
+              <li><a href="#">Food</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            
+
+            <div>
+                <a href="#"><h3 class="footer-heading mb-4">Connect With Us</h3></a>
+              <p>
+                <a href="https://www.facebook.com/moni.shankarnath.3"><span class="icon-facebook pt-2 pr-2 pb-2 pl-0"></span></a>
+                <a href="#"><span class="icon-twitter p-2"></span></a>
+                <a href="#"><span class="icon-instagram p-2"></span></a>
+                <a href="#"><span class="icon-rss p-2"></span></a>
+                <a href="#"><span class="icon-envelope p-2"></span></a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 text-center">
+            <p>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+
+  <script src="js/main.js"></script>
+<?php 
+      if (isset($_GET["submit"]))
+      {
+          $mail=$_GET["email"];
+          $feed=$_GET["message"];
+          $query="insert into feed(email,feedback) values('".$mail."','".$feed."')";
+          $fire = mysql_query($query,$conn) or die("Not Uploaded!! Sorry!!");
+          ?>
+      <div><h3><?php echo"Uploaded successfully";?></h3></div>
+      <?php
+      }
+      ?>
+ 
+  </body>
+</html>

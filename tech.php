f57d1c6f82de178acac48ce522e669c04eb8150c
Monishankar Nath <57766568+monishankarnath123@users.noreply.github.com>
2020-04-04 23:08:13 +0530

    Add files via upload

---
+++ tech.php
@@ -0,0 +1,228 @@
+ <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>TECH</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,700|Playfair+Display:400,700,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+  
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+    
+    <header class="site-navbar" role="banner">
+      <div class="container-fluid">
+        <div class="row align-items-center">
+          
+          <div class="col-12 search-form-wrap js-search-form">
+            <form method="get" action="#">
+              <input type="text" id="s" class="form-control" placeholder="Search...">
+              <button class="search-btn" type="submit"><span class="icon-search"></span></button>
+            </form>
+          </div>
+
+          <div class="col-4 site-logo">
+            <a href="index.html" class="text-black h2 mb-0">Moni's Blog</a>
+          </div>
+
+          <div class="col-8 text-right">
+            <nav class="site-navigation" role="navigation">
+              <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block mb-0">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="tech.php">Tech</a></li>
+                <li><a href="#">Entertainment</a></li>
+                <li><a href="#">Travel</a></li>
+                <li><a href="#">Family & Friends</a></li>
+                <li class="d-none d-lg-inline-block"><a href="#" class="js-search-toggle"><span class="icon-search"></span></a></li>
+              </ul>
+            </nav>
+            <a href="#" class="site-menu-toggle js-menu-toggle text-black d-inline-block d-lg-none"><span class="icon-menu h3"></span></a></div>
+          </div>
+
+      </div>
+    </header>
+    
+    
+    <div class="py-5 bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <span>Category</span>
+            <h3>TECH</h3>
+            <p>Category description here.. This page consist of all the technical reviews I've done on my different platforms.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section bg-white">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 mb-4">
+            <div class="entry2">
+              <a href="https://monishankarnath.blogspot.com/2018/08/samsung-galaxy-on7-pro-about-this-item.html"><img src="images/sam.jpg" alt="Image" class="img-fluid rounded"></a>
+              <div class="excerpt">
+              <span class="post-category text-white bg-secondary mb-3">Smart Phone</span>
+
+              <h2><a href="single.html">Samsung Galaxy ON7 pro.</a></h2>
+              <div class="post-meta align-items-center text-left clearfix">
+                <figure class="author-figure mb-0 mr-3 float-left"><img src="images/moni_1.jpg" alt="Image" class="img-fluid"></figure>
+                <span class="d-inline-block mt-1">By <a href="#">Monishankar Nath.</a></span>
+                <span>&nbsp;-&nbsp; August 25, 2018</span>
+              </div>
+              
+                <p>Surf Like a Pro
+Enjoy high-speed browsing with 4G enabled Galaxy On7 Pro. With Ultra Data Saving mode, save up to 50%* data as you shop online, stream videos or download your favourite apps.</p>
+                <p><a href="https://monishankarnath.blogspot.com/2018/08/samsung-galaxy-on7-pro-about-this-item.html">Read More</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 mb-4">
+            <div class="entry2">
+              <a href="https://youtu.be/ZEhdrUP1Z1c"><img src="images/m3.jpg" alt="Image" class="img-fluid rounded"></a>
+              <div class="excerpt">
+              <span class="post-category text-white bg-success mb-3">Smart Watch</span>
+
+              <h2><a href="single.html">M3 Band Smart Fitness Tracker Watch </a></h2>
+              <div class="post-meta align-items-center text-left clearfix">
+                <figure class="author-figure mb-0 mr-3 float-left"><img src="images/moni_1.jpg" alt="Image" class="img-fluid"></figure>
+                <span class="d-inline-block mt-1">By <a href="#">Monishankar Nath</a></span>
+                <span>&nbsp;-&nbsp; Dec 20, 2019
+
+
+</span>
+              </div>
+              
+                <p>M3 Band Smart Fitness Tracker Watch Heart Rate with Activity Tracker Waterproof Body Functions Like Steps Counter, Calorie Counter, Blood Pressure, Heart Rate Monitor LED</p>
+                <p><a href="https://youtu.be/ZEhdrUP1Z1c">Read More</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 mb-4">
+            <div class="entry2">
+              <a href="https://youtu.be/S3LSEk33TWc"><img src="images/webc.png" alt="Image" class="img-fluid rounded"></a>
+              <div class="excerpt">
+              <span class="post-category text-white bg-warning mb-3">Web Camera</span>
+
+              <h2><a href="single.html">QUANTUM WEB CAMERA.</a></h2>
+              <div class="post-meta align-items-center text-left clearfix">
+                <figure class="author-figure mb-0 mr-3 float-left"><img src="images/moni_1.jpg" alt="Image" class="img-fluid"></figure>
+                <span class="d-inline-block mt-1">By <a href="#">Monishankar Nath</a></span>
+                <span>&nbsp;-&nbsp; Mar 10, 2020
+
+
+</span>
+              </div>
+              
+                <p>Simply clip this 6 led webcam on your PC or laptop and start chatting without downloading any drivers. The web camera also has six lights that automatically.</p>
+                <p><a href="https://youtu.be/S3LSEk33TWc">Read More</a></p>
+              </div>
+            </div>
+          </div>
+
+
+          
+          </div>
+        </div>
+        <div class="row text-center pt-5 border-top">
+          <div class="col-md-12">
+            <div class="custom-pagination">
+              <span>1</span>
+              <a href="#">2</a>
+              <a href="#">3</a>
+              <a href="#">4</a>
+              <span>...</span>
+              <a href="#">15</a>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+    
+    
+    
+         
+    <div class="site-footer">
+      <div class="container">
+        <div class="row mb-5">
+         
+          <div class="col-md-4">
+            <!-- <h3 class="footer-heading mb-4">Navigation</h3> -->
+            <ul class="list-unstyled float-left mr-5">
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">My Career</a></li>
+            </ul>
+            <ul class="list-unstyled float-left">
+              <li><a href="#">Travel</a></li>
+              <li><a href="#">Food</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            
+
+            <div>
+                <a href="#"><h3 class="footer-heading mb-4">Connect With Us</h3></a>
+              <p>
+                <a href="https://www.facebook.com/moni.shankarnath.3"><span class="icon-facebook pt-2 pr-2 pb-2 pl-0"></span></a>
+                <a href="#"><span class="icon-twitter p-2"></span></a>
+                <a href="#"><span class="icon-instagram p-2"></span></a>
+                <a href="#"><span class="icon-rss p-2"></span></a>
+                <a href="#"><span class="icon-envelope p-2"></span></a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 text-center">
+            <p>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+
+  <script src="js/main.js"></script>
+
+    
+  </body>
+</html>
